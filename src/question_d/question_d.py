c61bd2378d12f1a22c13221ee5a2397407eb6839
Jesse Borunda <jesse.borunda@g.austincc.edu>
2025-10-18 19:54:25 +0000

    question d

--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,30 @@
-#write functions here, don't add input('') statements here!
+def get_day_of_week(day: int) -> str:
+	"""Return the weekday name for a number in 1..7.
+
+	1 -> Monday
+	2 -> Tuesday
+	3 -> Wednesday
+	4 -> Thursday
+	5 -> Friday
+	6 -> Saturday
+	7 -> Sunday
+
+	Raises ValueError if day is not in 1..7.
+	"""
+	days = {
+		1: "Monday",
+		2: "Tuesday",
+		3: "Wednesday",
+		4: "Thursday",
+		5: "Friday",
+		6: "Saturday",
+		7: "Sunday",
+	}
+	try:
+		return days[int(day)]
+	except (KeyError, ValueError, TypeError):
+		raise ValueError("Day must be an integer in the range 1..7")
+
+
+def test_config():
+	return True
