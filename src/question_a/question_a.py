a6784fdeb0b7b5ea61c9e558da691804d9bcfc7c
Jesse Borunda <jesse.borunda@g.austincc.edu>
2025-10-18 19:15:48 +0000

    Code draft

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,20 @@
-#write functions here, don't add input('') statements here!
-def test_config():
-    return True
+def get_assessment_value(value):
+    return value *0.06
+
+def get_tax_assessed(assessment_value):
+    return (assessment_value/100) *0.72
+
+#Main program
+def main():
+    actual_value = float(input("enter the actual value of the property: $"))
+
+    assessment_value = get_assessment_value(actual_value)
+
+    tax = get_tax_assessed(assessment_value)
+
+    #Display results
+    print(f"assessment value:  ${assessment_value:.2f}")
+    print{f"property tax: ${tax:.2f"}"
+
+#Run the program
+main()
